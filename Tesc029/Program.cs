b979eea8012e72ff73c8063afb167632787cd8c3
«Evgenia <evgenia.kopitext@gmail.com>
2022-07-05 21:06:06 +1100

    Tasc029

---
+++ Tesc029/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FullArray(int[] array)
+{
+    int lenght = array.Length;
+    int index = 0;
+    while (index<lenght)
+    {
+        array[index] = new Random().Next(1,100);
+        index++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int lenght = array.Length;
+    int index = 0;
+    while(index<lenght-1)
+    {
+        Console.Write(array[index] + ", ");
+        index++;
+    }
+    Console.Write(array[index]);
+
+}
+int []array = new int[8];
+FullArray(array);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
